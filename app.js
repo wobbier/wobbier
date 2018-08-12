a29f671ca06a900c6667467d5a57a447c87c7b8e
Mitchell Andrews <rastaninja77@gmail.com>
2018-08-12 13:36:12 -0400

    Switching to Metalsmith.io static generation

--- app.js
+++ app.js
@@ -1,91 +1,32 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var mu = require('mu2');
-var util = require('util');
-var path = require('path');
-var layout = require(__dirname + '/core/layout.js');
-var database = require(__dirname + '/core/database.js');
-var app = express();
+var Metalsmith  = require('metalsmith');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+var permalinks  = require('metalsmith-permalinks');
+var asset       = require('metalsmith-static');
+var date        = new Date();
 
-//add some standard express middleware
-app.use(logfmt.requestLogger());
-app.use(express.static(__dirname + '/public'));
-mu.root = __dirname + '/views';
-mu.clearCache();
-
-app.get('/', function (req, res) {
-    var index = require(__dirname + '/data/index.js');
-    index.loadData(function (data) {
-        layout.render(res, 'index.ms', data, function (stream) {
-            stream.pipe(res);
-        });
-    });
-});
-
-app.get('/game/:id', function (req, res) {
-    var id = req.params.id;
-    console.log(id);
-    var game = require(__dirname + '/data/index.js');
-    game.getGame(id, function (data) {
-        if (data.external === true) {
-            res.redirect(data.url);
-            return;
-        }
-        layout.render(res, 'game.ms', data, function (stream) {
-            stream.pipe(res);
-        });
-    });
-});
-
-app.get('/blog', function (req, res) {
-    var blog = require(__dirname + '/data/blog.js');
-    blog.loadPosts(function (data) {
-        layout.render(res, 'blog.ms', data, function (stream) {
-            stream.pipe(res);
-        });
-    });
-});
-
-app.get('/blog/:id', function (req, res) {
-    var id = req.params.id;
-    var blog = require(__dirname + '/data/blog.js');
-    blog.loadPost(id, function (data) {
-        layout.render(res, 'post.ms', data, function (stream) {
-            stream.pipe(res);
-        });
-    });
-});
-
-app.post('/message', function (req, res) {
-    var nodemailer = require('nodemailer');
-
-    var colors = ['249,87,48', '254,55,74', '110,167,60', '90,177,168', '186,40,51', '206,76,44', '52,130,82'];
-    var color = colors[Math.floor(Math.random() * colors.length)];
-
-    var transporter = nodemailer.createTransport();
-
-    transporter.sendMail({
-        from: 'noreply@wobbier.com',
-        to: 'rastaninja77@live.com',
-        subject: 'Message from wobbier.com',
-        html: '<html><style type="text/css">a{color:#fff}</style><body style="background-color: rgb(' + color + '); color:#fff;margin:0; padding:20px;"><img src="http://wobbier.com/img/wobbier-logo.png" style="margin:0 auto; text-align: center;" /><hr style="border-color: #fff;" />' + req.body.message + '<br /><br />From, <br />' + req.body.email + '</body></html>'
-    });
-    res.write('Thank you for the message!<br> I\'ll try to get back to you soon :)');
-    res.end();
-});
-
-app.get('/css/theme.css', function (req, res) {
-    var colors = ['249,87,48', '254,55,74', '110,167,60', '90,177,168', '186,40,51', '206,76,44', '52,130,82'];
-    var color = colors[Math.floor(Math.random() * colors.length)];
-    res.writeHeader(200, {
-        "Content-Type": "text/css"
-    });
-    res.write("body, .block{background: rgb(" + color + ")}");
-    res.end();
-});
-
-var port = Number(process.env.PORT || 9292);
-app.listen(port, function () {
-    console.log("Listening on " + port);
-});
+Metalsmith(__dirname)
+  .metadata({
+    title: "Mitch Andrews | Game Developer",
+    description: "The home of Mitch Andrews!",
+    year: date.getFullYear(),
+    url: "http://wobbier.github.io/"
+  })
+  .source('./src')
+  .destination('./build')
+  .clean(false)
+  .use(markdown())
+  .use(permalinks({
+    relative: false
+  }))
+  .use(layouts({
+    engine: 'handlebars'
+  }))
+  .use(asset({
+    "src": "public",
+    "dest": ".",
+    "createDest": true
+  }))
+  .build(function(err, files) {
+    if (err) { throw err; }
+  });
---
+++ src/index.md
@@ -0,0 +1,78 @@
+---
+layout: layout.html
+---
+<div class="home" style="text-align:center;">
+    <div class="block top left"></div>
+    <div class="block bottom left"></div>
+    <div class="block top right"></div>
+    <div class="block bottom right"></div>
+    <p>My name is Mitch Andrews and I'm a game programmer from
+        <strong>Toronto</strong>, Canada.</p>
+    <p>I program primarily in
+        <strong>C++</strong>,
+        <strong>C#</strong>, and
+        <strong>Node.js</strong> using either OS X, Windows, or Linux.</p>
+    <p>I primarily use C++, Unity, and Unreal Engine 4 for game development.</p>
+    <p>Contact me
+        <a href="#contact">at the bottom</a> if you have any questions!</p>
+</div>
+<div class="account">
+    <div class="box section">
+        <div class="block top left"></div>
+        <div class="block bottom left"></div>
+        <div class="block top right"></div>
+        <div class="block bottom right"></div>
+        <a href="https://github.com/wobbier?tab=repositories">
+            <img src="img/GitHub-Mark.png" height="64px">
+        </a>
+    </div>
+    <div class="box section last">
+        <div class="block top left"></div>
+        <div class="block bottom left"></div>
+        <div class="block top right"></div>
+        <div class="block bottom right"></div>
+        <a href="https://ca.linkedin.com/in/mitchellandrews">
+            <img src="img/linkedin.png">
+        </a>
+    </div>
+    <div class="clearfix"></div>
+</div>
+<div id="gamesHeader">
+    <img src="img/portfolio.png"></img>
+</div>
+<!--div class="games">
+    <div class="game">
+        <div class="gameThumb">
+            <div class="block top left"></div>
+            <div class="block bottom left"></div>
+            <div class="block bottom right"></div>
+            <a href="{{url}}">
+                <img src="{{thumb}}" />
+            </a>
+            <a href="/game/{{slug}}">
+                <img src="{{thumb}}" />
+            </a>
+        </div>
+        <div class="gameTitle {{^thumb}}noThumb{{/thumb}}">
+            {{^thumb}}
+            <div class="block top left"></div>
+            <div class="block bottom left"></div>
+            {{/thumb}}
+            <div class="block top right"></div>
+            <div class="block bottom right"></div>
+            <h1>
+                <a href='{{#external}}{{url}}{{/external}}{{^external}}/game/{{slug}}{{/external}}'>{{title}}</a> -
+                <span class="date">{{date}}</span>
+            </h1>
+        </div>
+        <div class="gameContent {{^thumb}}noThumb{{/thumb}}">
+            <div class="block top left"></div>
+            <div class="block bottom left"></div>
+            <div class="block top right"></div>
+            <div class="block bottom right"></div>
+            <p>{{{description}}}</p>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    {{/hidden}} {{/games}}
+</div-->
